245e17341cbb1a256d07993ab84ac750b961113f
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-05-20 23:43:12 +0500

    описал часть шагов для тестирования товаров

--- src/test/java/swagLabs/steps/ShopSteps.java
+++ src/test/java/swagLabs/steps/ShopSteps.java
@@ -1,15 +1,42 @@
 package swagLabs.steps;
 
+import com.codeborne.selenide.Condition;
 import io.qameta.allure.Step;
-import swagLabs.pages.AuthPage;
-import swagLabs.pages.ShopPage;
+import swagLabs.pages.GeneralShopPage;
+
+import java.util.List;
+
+import static com.codeborne.selenide.Condition.*;
 
 public class ShopSteps {
-    private AuthPage authPage = new AuthPage();
-    private ShopPage shopPage = new ShopPage();
+    private GeneralShopPage shopPage = new GeneralShopPage();
 
     @Step
-    public void addedProductToCart() {
+    //Добавление продукта в корзину
+    public void addSingleProductToCart(String productName) {
+        shopPage.fullBlockOnceProduct()
+                .filter(cssClass("inventory_item_name"))
+                .findBy(exactText(productName));
+        productName = productName.replace(" ", "-").toLowerCase();
+        shopPage.addToCartButton()
+                .findBy(attribute("name", "add-to-cart" + "-" + productName))
+                .click();
+    }
+    @Step
+    //Открытие корзины
+    public void openCart() {
+        shopPage.cartButton().click();
+    }
+    @Step
+    //Проверка наличия продукта в корзине
+    public void checkHaveItemInCart(String itemName) {
+        shopPage.itemsInCart()
+                .findBy(attribute("class"))
+                .shouldHave(exactText(itemName));
+    }
+    @Step
+    //Проверка наличия продуктов в корзине
+    public void checkHaveItemInCart(List<String> itemNames) {
 
     }
 }
