033526679d42eced14ea0464efe9d5ca16067874
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-05-20 23:42:55 +0500

    добавил тест + 1 заготовку для теста

--- src/test/java/swagLabs/tests/ShopTests.java
+++ src/test/java/swagLabs/tests/ShopTests.java
@@ -1,17 +1,46 @@
 package swagLabs.tests;
 
+import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import swagLabs.config.SelenideConfiguration;
-import swagLabs.helpers.HelpersMethod;
+import swagLabs.config.ShopPageConfiguration;
+import swagLabs.helpers.DataForTests;
+import swagLabs.steps.ShopSteps;
 
-import java.util.List;
-
-@ExtendWith(SelenideConfiguration.class)
+@ExtendWith(ShopPageConfiguration.class)
 @DisplayName("Tests for shop page")
 public class ShopTests {
-    private final String password = HelpersMethod.getPassword();
-    private final List<String> listLogins = HelpersMethod.getLogins();
+    private ShopSteps shopSteps = new ShopSteps();
+    private DataForTests dataForTests = new DataForTests();
 
+    /***
+     * 1. Open shop page;
+     * 2. Add item;
+     * 3. Open cart;
+     * 4. Check have item.
+     */
+    @Test
+    @DisplayName("Add single item from general page to cart")
+    @Epic("UI")
+    @Story("ShopPage")
+    @Severity(SeverityLevel.NORMAL)
+    @Owner("Mikhail Salnikov")
+    @Issue("Shop")
+    void addOnceItemToCart() {
+        shopSteps.addSingleProductToCart(dataForTests.getNameSingleItem());
+        shopSteps.openCart();
+        shopSteps.checkHaveItemInCart(dataForTests.getNameSingleItem());
+    }
 
+    @Test
+    @DisplayName("Add multiple item from general page to cart")
+    @Epic("UI")
+    @Story("ShopPage")
+    @Severity(SeverityLevel.NORMAL)
+    @Owner("Mikhail Salnikov")
+    @Issue("Shop")
+    public void addMultipleItem() {
+
+    }
 }
